ab2f6d8efcfc33ab65b59b12bd436199ada115ae
thiasgo <106076109+tujarao@users.noreply.github.com>
2024-05-10 09:17:40 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -11,3 +11,36 @@ for (let i = 0; i < botoes.length; i++){
         textos[i].classList.add("ativo")
     }
 }
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2025-02-30T23:59:59");
+const tempoObjetivo2 = new Date("2024-07-30T23:59:59");
+const tempoObjetivo3 = new Date("2030-01-01T23:59:59");
+const tempoObjetivo4 = new Date("2036-06-30T23:59:59");
+const tempoObjetivo5 = new Date("2024-07-30T23:59:59");
+
+const tempos = [
+    tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4, tempoObjetivo5
+]
+
+for (let i=0; i< contadores.length; i++){
+    contadores[i].textContent = calculaTempo(tempos[i]);
+}
+contadores[0].textContent = calculaTempo(tempoObjetivo1)
+
+function calculaTempo(tempoObjetivo1){
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo1 - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+    return dias + "dias"
+        + horas + "horas"
+        + minutos + "minutos"
+        + segundos + "segundos";
+}
+
